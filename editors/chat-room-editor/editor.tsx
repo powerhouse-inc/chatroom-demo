8077dd1e1aef3e49d75887a4e8c51be554304e27
Guillermo Puente <guillermo.ps09@gmail.com>
2024-11-29 09:10:19 -0300

    migrated utils

--- editors/chat-room-editor/editor.tsx
+++ editors/chat-room-editor/editor.tsx
@@ -4,88 +4,18 @@ import {
   ChatRoomState,
   ChatRoomAction,
   ChatRoomLocalState,
-  Message,
   ReactionType,
   actions,
 } from "../../document-models/chat-room";
 import { utils as documentModelUtils } from "document-model/document";
-import {
-  ChatRoom,
-  ChatRoomProps,
-  MessageProps,
-  reactionMap,
-} from "./components";
+import { ChatRoom, ChatRoomProps, MessageProps } from "./components";
+import { reactionKeyToReactionType, mapReactions } from "./utils";
 
 export type IProps = EditorProps<
   ChatRoomState,
   ChatRoomAction,
   ChatRoomLocalState
 >;
-
-const reactionTypeToEmoji = (reactionType: ReactionType): string => {
-  switch (reactionType) {
-    case "HEART":
-      return "❤️";
-    case "THUMBS_UP":
-      return "👍";
-    case "THUMBS_DOWN":
-      return "👎";
-    case "LAUGH":
-      return "😂";
-    case "CRY":
-      return "😢";
-    default:
-      return "❤️";
-  }
-};
-
-const reactionTypeToReactionKey = (
-  reactionType: ReactionType,
-): keyof typeof reactionMap => {
-  switch (reactionType) {
-    case "HEART":
-      return "heart";
-    case "THUMBS_UP":
-      return "thumbsUp";
-    case "THUMBS_DOWN":
-      return "thumbsDown";
-    case "LAUGH":
-      return "laughing";
-    case "CRY":
-      return "cry";
-    default:
-      return "heart";
-  }
-};
-
-const reactionKeyToReactionType = (reactionKey: string): ReactionType => {
-  switch (reactionKey) {
-    case "heart":
-      return "HEART";
-    case "thumbsUp":
-      return "THUMBS_UP";
-    case "thumbsDown":
-      return "THUMBS_DOWN";
-    case "laughing":
-      return "LAUGH";
-    case "cry":
-      return "CRY";
-    default:
-      return "HEART";
-  }
-};
-
-const mapReactions = (
-  reactions: Message["reactions"],
-): MessageProps["reactions"] => {
-  return (reactions || [])
-    .map((reaction) => ({
-      emoji: reactionTypeToEmoji(reaction.type),
-      reactedBy: reaction.reactedBy,
-      type: reactionTypeToReactionKey(reaction.type),
-    }))
-    .filter((reaction) => reaction.reactedBy.length > 0);
-};
 
 export default function Editor(props: IProps) {
   const disableChatRoom = !props.context.user;
@@ -179,12 +109,14 @@ export default function Editor(props: IProps) {
       }}
     >
       <ChatRoom
-        description={props.document.state.global.description || undefined}
+        description={
+          props.document.state.global.description || "This is a chat room demo"
+        }
         disabled={disableChatRoom}
         messages={messages}
         onClickReaction={onClickReaction}
         onSendMessage={onSendMessage}
-        title={props.document.state.global.name}
+        title={props.document.state.global.name || "Chat Room Demo"}
       />
     </div>
   );
--- editors/chat-room-editor/utils/index.ts
+++ editors/chat-room-editor/utils/index.ts
@@ -1,3 +1,6 @@
+import { MessageProps, reactionMap } from "../components";
+import { Message, ReactionType } from "../../../document-models/chat-room";
+
 const emojis = [
   "😀",
   "😂",
@@ -56,3 +59,70 @@ export function getEmojiFromString(input: string): string {
   const hash = hashString(input);
   return emojis[hash % emojis.length];
 }
+
+export const reactionTypeToEmoji = (reactionType: ReactionType): string => {
+  switch (reactionType) {
+    case "HEART":
+      return "❤️";
+    case "THUMBS_UP":
+      return "👍";
+    case "THUMBS_DOWN":
+      return "👎";
+    case "LAUGH":
+      return "😂";
+    case "CRY":
+      return "😢";
+    default:
+      return "❤️";
+  }
+};
+
+export const reactionTypeToReactionKey = (
+  reactionType: ReactionType,
+): keyof typeof reactionMap => {
+  switch (reactionType) {
+    case "HEART":
+      return "heart";
+    case "THUMBS_UP":
+      return "thumbsUp";
+    case "THUMBS_DOWN":
+      return "thumbsDown";
+    case "LAUGH":
+      return "laughing";
+    case "CRY":
+      return "cry";
+    default:
+      return "heart";
+  }
+};
+
+export const reactionKeyToReactionType = (
+  reactionKey: string,
+): ReactionType => {
+  switch (reactionKey) {
+    case "heart":
+      return "HEART";
+    case "thumbsUp":
+      return "THUMBS_UP";
+    case "thumbsDown":
+      return "THUMBS_DOWN";
+    case "laughing":
+      return "LAUGH";
+    case "cry":
+      return "CRY";
+    default:
+      return "HEART";
+  }
+};
+
+export const mapReactions = (
+  reactions: Message["reactions"],
+): MessageProps["reactions"] => {
+  return (reactions || [])
+    .map((reaction) => ({
+      emoji: reactionTypeToEmoji(reaction.type),
+      reactedBy: reaction.reactedBy,
+      type: reactionTypeToReactionKey(reaction.type),
+    }))
+    .filter((reaction) => reaction.reactedBy.length > 0);
+};
